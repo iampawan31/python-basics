0de59f6cb4e39d43802911146389718f6adad67d
Pawan Kumar <surpawan@gmail.com>
2020-07-19 20:04:26 +0530

    Added Variables and Strings

---
+++ variables.py
@@ -0,0 +1,21 @@
+x = 1               # int
+y = 2.5             # float
+name = 'Pawan'      # str
+is_cool = True      # bool
+
+# Multiple Assignment
+x, y, name, is_cool = (1, 2.5, 'Pawan', True)
+
+# Basic math
+a = x + y
+
+# Casting
+x = str(x)
+y = int(y)
+z = float(y)
+
+print('Hello World')
+print(x, y, name, is_cool, a)
+print(type(x))
+print(type(y), y)
+print(type(z), z)
